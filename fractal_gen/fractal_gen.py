9b004bcedd53828cbc389ef5850d88c864340046
Neil Bostian <neil.bostian@gmail.com>
2019-10-20 00:54:08 -0400

    fixes

--- fractal_gen/fractal_gen.py
+++ fractal_gen/fractal_gen.py
@@ -2,48 +2,18 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
+R = 4
+ITER_NUM = 300
+
 class FractalGenTensorflowModel():
     def __init__(self, shape_in):
         self._sess = tf.Session()
 
-        self._R = 4
-        self._ITER_NUM = 200
-
-        self._build_model(shape_in)
-
-        self._sess.run(tf.global_variables_initializer())
-
-        tf.summary.FileWriter('tb', self._sess.graph)
-
-    def _build_model(self, shape_in):
-        self._in_xs = tf.placeholder(shape=shape_in, dtype=tf.float32)
-        self._in_zs = tf.placeholder(shape=shape_in, dtype=tf.float32)
-
-        xs = self._in_xs
-        zs = self._in_zs
-        
-        ns = tf.zeros_like(xs, tf.float32)
-
-        def apply_loop(x_in, z_in, n_in):
-            z_out = tf.where(tf.abs(z_in) < self._R, z_in**2 + x_in, z_in)
-            n_delta = tf.abs(z_out) < self._R
-            n_out = n_in + tf.cast(n_delta, tf.float32)
-            return (z_out, n_out)
-
-        for i in range(0, self._ITER_NUM):
-            with tf.name_scope(f'iter{i}'):
-                (z_out, n_out) = apply_loop(xs, zs, ns)
-                zs = z_out
-                ns = n_out
-
-        self._out_final_z = zs
-        self._out_final_step = ns
-
     def _get_color(self, bg_ratio, ratio):
         def color(z, i):
-            if abs(z) < self._R:
+            if abs(z) < R:
                 return 0, 0, 0
-            v = np.log2(i + self._R - np.log2(np.log2(abs(z)))) / 5
+            v = np.log2(i + R - np.log2(np.log2(abs(z)))) / 5
             if v < 1.0:
                 return v**bg_ratio[0], v**bg_ratio[1], v ** bg_ratio[2]
             else:
@@ -52,10 +22,29 @@ class FractalGenTensorflowModel():
         return color
 
     def generate_image(self, Z, c, bg_ratio, ratio):
-        final_z, final_step = self._sess.run([self._out_final_z, self._out_final_step], feed_dict={
-            self._in_xs: np.full(shape=Z.shape, fill_value=c, dtype=Z.dtype),
-            self._in_zs: Z,
-        })
+        sess = self._sess
+        print('init1')
+        g = np.full(shape=Z.shape, fill_value=c, dtype=Z.dtype)
+        print('init2')
+        xs = tf.Variable(g)
+        zs = tf.Variable(Z)
+        ns = tf.Variable(tf.zeros_like(xs, tf.float32))
+
+        zs_ = tf.where(tf.abs(zs) < R, zs**2 + xs, zs)
+        not_diverged = tf.abs(zs_) < R
+        step = tf.group(
+            zs.assign(zs_),
+            ns.assign_add(tf.cast(not_diverged, tf.float32))
+        )
+
+        sess.run(tf.global_variables_initializer())
+
+        for i in range(ITER_NUM):
+            print(f'\titer{i}')
+            sess.run(step)
+
+        final_step = sess.run(ns)
+        final_z = sess.run(zs_)
 
         r, g, b = np.frompyfunc(self._get_color(bg_ratio, ratio), 2, 3)(final_z, final_step)
 
@@ -66,11 +55,12 @@ class FractalGenTensorflowModel():
 class FractalGenModel():
     def __init__(self):        
         #input params
-        self.start_x = -1.9
-        self.end_x = 1.9
-        self.start_y = -1.1
-        self.end_y = 1.1
-        self.width = 600 # image width
+        ratio = 16.0 / 9.0
+        self.start_y = -.7
+        self.end_y = .7
+        self.start_x = self.start_y * ratio
+        self.end_x = self.end_y * ratio
+        self.width = 1920 # image width 7680
         self.bg_ratio = (1, 1, 1) # background color ratio
         self.ratio = (0.9, 0.9, 0.9)
 
@@ -80,10 +70,11 @@ class FractalGenModel():
 
         self._tfmodel = FractalGenTensorflowModel(self._Z.shape)
 
-    def generate_sequence(self, iters=60):
+    def generate_sequence(self, iters=300):
         for i in range(0, iters):
             theta = 2 * np.pi / iters * i
             c = -(0.835 - 0.1 * np.cos(theta)) - (0.2321 + 0.1 * np.sin(theta)) * 1j
+            # c = -0.8 * 1j
             y = self._tfmodel.generate_image(self._Z, c, self.bg_ratio, self.ratio)
             print(f'Fractal gen completed {i+1}/{iters}')
             yield y
